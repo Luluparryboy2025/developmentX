d479c3840458aff3983e9cdd25385028d0785082
Luluparryboy2025 <luluparryboy@gmail.com>
2025-06-30 12:47:55 +1200

    index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Development X" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Welcome to the DevelopmentX Handbook!
         </p>
       </main>
 
